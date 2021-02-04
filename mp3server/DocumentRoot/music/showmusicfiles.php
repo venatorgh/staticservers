a18e623aa9d24009cd173f4ab4b34f0fe01bcd25
George Hanson <gahanson@gmail.com>
2021-02-04 12:45:00 -0600

    first commit

---
+++ mp3server/DocumentRoot/music/showmusicfiles.php
@@ -0,0 +1,18 @@
+<?php
+
+function getDirContents($path) {
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+    $files = array(); 
+    foreach ($rii as $file)
+        if (!$file->isDir())
+            $files[] = $file->getPathname();
+
+    return $files;
+}
+
+header("Access-Control-Allow-Origin: *");
+
+echo json_encode(getDirContents("./artists"));
+
+?>
